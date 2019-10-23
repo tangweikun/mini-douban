c7dfe2b527aea51775db5ecad6c58f4a2158adb5
tangweikun <819573105@qq.com>
2019-10-23 15:41:58 +0800

    :construction: work on

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,36 +1,18 @@
-var start = 0;
 Page({
-  start: 0,
   data: {
+    start: 0,
     movies: [],
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function(options) {
-    var that = this;
-
-    this.loadMore(that);
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-    start = 0;
-    var that = this;
-    this.loadMore(that);
-    wx.stopPullDownRefresh();
+  onLoad: function() {
+    this._loadMore();
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function() {
-    var that = this;
-
-    this.loadMore(that);
+    this._loadMore();
   },
 
   /**
@@ -38,36 +20,26 @@ Page({
    */
   onShareAppMessage: function() {},
 
-  loadMore: function(that) {
-    that.getMovie(function(d) {
-      //回调函数，根据数据设置页面data，更新到视图
-      wx.hideToast(); //隐藏加载框
-      var list = that.data.movies;
-      for (var i = 0; i < d.length; i++) {
-        list.push(d[i]);
-      }
-      that.setData({ movies: list }); //更新数据，视图将同步更新
-    });
-  },
-
-  getMovie: function(fn) {
-    wx.showToast({ title: '加载中', icon: 'loading', duration: 10000 });
+  _loadMore: function() {
+    const that = this;
+    wx.showToast({ title: '加载中', icon: 'loading', duration: 2000 });
     wx.request({
-      url: 'https://douban.uieee.com/v2/movie/top250?start=' + start,
+      url: 'https://douban.uieee.com/v2/movie/top250?start=' + that.data.start,
       header: { 'content-type': 'json' },
       success: function(res) {
-        start = start + 20;
-        console.log(res);
-        fn(res.data.subjects);
+        that.setData({
+          movies: [...that.data.movies, ...res.data.subjects],
+          start: that.data.start + 20,
+        });
       },
     });
   },
 
-  goDetail: function(e) {
-    var that = this;
+  _goDetail: function(e) {
     //拿到点击的index下标
-    var index = e.currentTarget.dataset.index;
-    var movie = JSON.stringify(that.data.movies[index]);
+    const index = e.currentTarget.dataset.index;
+    console.log(this.data.movies[index]);
+    const movie = JSON.stringify(this.data.movies[index]);
     wx.navigateTo({
       url: '../detail/detail?movie=' + movie,
     });
